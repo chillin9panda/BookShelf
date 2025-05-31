f0997a6ad492ba058a2b3d18952a007b3fadb013
chillin9panda <duressamarkos9@gmail.com>
2025-05-31 08:26:38 +0300

    hot reload setup

--- Program.cs
+++ Program.cs
@@ -17,18 +17,21 @@ builder.Services
     .AddDefaultIdentity<IdentityUser>(
         options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-  app.UseMigrationsEndPoint();
-} else {
-  app.UseExceptionHandler("/Home/Error");
-  // The default HSTS value is 30 days. You may want to change this for
-  // production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for
+    // production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();

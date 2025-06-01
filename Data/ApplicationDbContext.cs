248f0448b414f740841a37e0d6ca0f4ee645d519
chillin9panda <duressamarkos9@gmail.com>
2025-06-01 18:56:29 +0300

    removed Author model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ namespace BookShelf.Data;
 public class ApplicationDbContext : IdentityDbContext
 {
     public DbSet<Book> Books { get; set; }
-    public DbSet<Author> Author { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
 }

2243607bc69eff79efe5d70345c5fbfe096c65ae
chillin9panda <duressamarkos9@gmail.com>
2025-06-01 18:55:52 +0300

    add book form submission, save to db

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using BookShelf.Models;
+using BookShelf.Data;
 
 namespace BookShelf.Controllers
 {
+    // Route Book
     public class BookController : Controller
     {
         public IActionResult Overview()
@@ -15,6 +17,51 @@ namespace BookShelf.Controllers
                 Genre = Genre.Programming
             };
             return View(book);
+        }
+
+        private readonly ApplicationDbContext context;
+
+        public BookController(ApplicationDbContext _context) { context = _context; }
+
+        public async Task<IActionResult> AddBook(BookViewModel model)
+        {
+            try
+            {
+
+                if (model.BookFile != null && model.BookFile.Length > 0)
+                {
+                    var fileName = Path.GetFileName(model.BookFile.FileName);
+                    var filePath = Path.Combine("wwwroot/BookFiles", fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await model.BookFile.CopyToAsync(stream);
+                    }
+
+                    var saveBook = new Book
+                    {
+                        Title = model.Title,
+                        Description = model.Description,
+                        Genre = model.Genre,
+                        Author = model.Author,
+                        PublicationDate = model.PublicationDate,
+                        BookFilePath = "/BookFiles/" + fileName
+                    };
+
+                    context.Books.Add(saveBook);
+                    await context.SaveChangesAsync();
+
+                    return RedirectToAction("Overview");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error saving book: {e.Message}");
+                ModelState.AddModelError("", "Error Saving book");
+            }
+
+            return View(model);
         }
     }
 
---
+++ Models/BookViewModel.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using BookShelf.Models;
+
+public class BookViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Genre Genre { get; set; }
+    public string Author { get; set; }
+    public DateOnly PublicationDate { get; set; }
+    public IFormFile BookFile { get; set; }
+}
